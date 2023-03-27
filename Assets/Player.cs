72eaf2a701396a07c9dfd88942b17d972653ae64
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2023-03-27 11:27:41 -0400

    Updated the Player.cs file with correct documentation

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,3 +1,9 @@
+/*
+ * BreakoutBlast by Jackson Nevins
+ * Remake of Brick breaker with 3d assets and a locked camera perspective. Connects to MariaDB to keep track of scores and who obtained the score.
+ * 
+ */
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,19 +12,19 @@ public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f; // player's movement speed
 
-    private Rigidbody rb;
-    private Camera mainCamera;
+    private Rigidbody rb; // Ridgidbody connector
+    private Camera mainCamera; // Camera connector
 
     private void Start()
     {
-        rb = GetComponent<Rigidbody>();
-        mainCamera = FindObjectOfType<Camera>();
+        rb = GetComponent<Rigidbody>(); //connect the rb to the actual RidgidBody we want
+        mainCamera = FindObjectOfType<Camera>(); //connect the camera connector to the Main Camera
     }
 
-    private void FixedUpdate()
+    private void FixedUpdate() // Fixed update runs every frame and ignores the frame rate
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(horizontalInput, 0f, 0f);
+        float horizontalInput = Input.GetAxis("Horizontal"); 
+        Vector3 movement = new Vector3(horizontalInput, 0f, 0f); // only allows horizontal movement for the player
         rb.velocity = movement * moveSpeed;
 
         // Clamp the player's position to the edges of the screen
