4f32f30fa7461ec175e6da2485a8f126f2965a53
MinervaArgus <71613767+MinervaArgus@users.noreply.github.com>
2023-05-01 13:48:19 -0400

    Fixed DB Implementation

--- Assets/Scripts/SQLiteManager.cs
+++ Assets/Scripts/SQLiteManager.cs
@@ -6,38 +6,26 @@ using Mono.Data.Sqlite;
 
 public class SQLiteManager : MonoBehaviour
 {
-    private string dbUri;
+    private string dbUri = "URI=file:HighScores.db";
 
     void Start()
     {
-        CreateAndOpenDB();
+        IDbConnection dbConnection = CreateAndOpenDB();
+        dbConnection.Close();
     }
 
     private IDbConnection CreateAndOpenDB()
     {
-        dbUri = "URI=file:Scores.sqlite"; // 4
-        IDbConnection dbConnection = new SqliteConnection(dbUri); // 5
-        dbConnection.Open(); // 6
+        string dbUri = "URI=file:HighScores.db";
+        IDbConnection dbConnection = new SqliteConnection(dbUri);
+        dbConnection.Open();
 
-        // Create a table for the hit count in the database if it does not exist yet.
-        IDbCommand dbCommandCreateTable = dbConnection.CreateCommand(); // 6
-        dbCommandCreateTable.CommandText = "CREATE TABLE IF NOT EXISTS HitCountTableSimple (id INTEGER PRIMARY KEY, hits INTEGER )"; // 7
-        dbCommandCreateTable.ExecuteReader(); // 8
+        IDbCommand dbCommandCreateTable = dbConnection.CreateCommand();
+        dbCommandCreateTable.CommandText = "CREATE TABLE IF NOT EXISTS HighScores ( name VARCHAR(20), score INTEGER )";
+        dbCommandCreateTable.ExecuteNonQuery();
 
         return dbConnection;
     }
 
-    public void SaveScore(string name, int score)
-    {
-        IDbConnection dbConnection = new SqliteConnection(dbUri);
-        dbConnection.Open();
-
-        IDbCommand dbCommand = dbConnection.CreateCommand();
-        dbCommand.CommandText = "INSERT INTO scores (name, score) VALUES (@name, @score)";
-        dbCommand.Parameters.Add(new SqliteParameter("@name", name));
-        dbCommand.Parameters.Add(new SqliteParameter("@score", score));
-        dbCommand.ExecuteNonQuery();
-
-        dbConnection.Close();
-    }
+    
 }
